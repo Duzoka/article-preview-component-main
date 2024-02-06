ab2cde83d0d24f7f4c7f0452809056b3cf52c9c8
Eduardo S. Mesquita <102036752+Duzoka@users.noreply.github.com>
2024-02-06 17:13:29 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@ function chamar() {
 
    if (shareIcon.style.visibility == "hidden") {
     shareIcon.style.visibility = "visible";
-    shareIcon.style.animation = "minhaAnimacao 3s 1";
    } else {
     shareIcon.style.visibility = "hidden";
    }
